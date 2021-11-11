1f3dd85d4db93d51eb4139b78bb5a81ecc6657d6
Adham <adhamahmad541@gmail.com>
2021-11-11 19:37:19 +0200

    a7a

--- src/App.java
+++ src/App.java
@@ -1,3 +1,20 @@
+class Parser {
+String commandName;
+String[] args;
+
+public boolean parse(String input){
+
+    return true;
+}
+ public String getCommandName(){
+    return "a"; 
+}
+ public String[] getArgs(){
+    return "a";
+}
+
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
